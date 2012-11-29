c1cc492750daadf2dce5c19fd13a473cebe8c134
kumabotz <kumabotz@gmail.com>
2012-11-29 15:05:30 +0800

    Initial commit

---
+++ src/org/andengine/examples/ExampleLauncher.java
@@ -0,0 +1,46 @@
+package org.andengine.examples;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ExampleLauncher extends ListActivity {
+  final Example[] EXAMPLES = Example.values();
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // create an ArrayAdapter, that will actually make the Strings above appear
+    // in the ListView
+    setListAdapter(new ArrayAdapter<Example>(this,
+        android.R.layout.simple_list_item_1, EXAMPLES));
+  }
+
+  @Override
+  protected void onListItemClick(final ListView l, final View v, final int position, final long id) {
+    super.onListItemClick(l, v, position, id);
+    startActivity(new Intent(this, EXAMPLES[position].CLASS));
+  }
+
+  private static enum Example {
+    EXAMPLE_MENU(ExampleMenu.class, "Menu Example");
+
+    public final Class<? extends Activity> CLASS;
+    public final String NAME;
+
+    private Example(final Class<? extends Activity> pExampleClass,
+        final String pExampleName) {
+      CLASS = pExampleClass;
+      NAME = pExampleName;
+    }
+
+    @Override
+    public String toString() {
+      return NAME;
+    }
+  }
+}
