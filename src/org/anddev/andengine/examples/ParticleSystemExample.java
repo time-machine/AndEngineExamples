91aa38ff17685b997d1137fa8d7010af51720e6e
kumabotz <kumabotz@gmail.com>
2012-12-02 10:31:14 +0800

    Add particle system example

--- src/org/anddev/andengine/examples/ParticleSystemExample.java
+++ src/org/anddev/andengine/examples/ParticleSystemExample.java
@@ -1,33 +1,67 @@
 package org.anddev.andengine.examples;
 
+import javax.microedition.khronos.opengles.GL10;
+
 import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.FPSCounter;
 import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.particle.ParticleSystem;
+import org.anddev.andengine.entity.particle.modifier.AccelerationModifier;
+import org.anddev.andengine.entity.particle.modifier.ExpireModifier;
+import org.anddev.andengine.entity.particle.modifier.VelocityModifier;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.TextureRegion;
+import org.anddev.andengine.opengl.texture.TextureRegionFactory;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 public class ParticleSystemExample extends BaseGameActivity {
+  private static final int CAMERA_WIDTH = 720;
+  private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
+  private Texture mTexture;
+  private TextureRegion mFaceTextureRegion;
 
   @Override
   public Scene onLoadScene() {
-    // TODO Auto-generated method stub
-    return null;
+    getEngine().registerPreFrameHandler(new FPSCounter());
+
+    final Scene scene = new Scene(1);
+    scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
+
+    final ParticleSystem particleSystem = new ParticleSystem(0, CAMERA_HEIGHT, 0, 0,
+        5, 10, 200, mFaceTextureRegion);
+    particleSystem.addParticleModifier(new VelocityModifier(20, 30, -80, -120));
+    particleSystem.addParticleModifier(new AccelerationModifier(10, 20));
+    particleSystem.addParticleModifier(new ExpireModifier(15, 20));
+    scene.getTopLayer().addEntity(particleSystem);
+
+    return scene;
   }
 
   @Override
   public void onLoadResources() {
-    // TODO Auto-generated method stub
-
+    mTexture = new Texture(32, 32, new TextureOptions(GL10.GL_NEAREST,
+        GL10.GL_LINEAR, GL10.GL_MODULATE, GL10.GL_CLAMP_TO_EDGE,
+        GL10.GL_CLAMP_TO_EDGE));
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "gfx/boxface.png", 0, 0);
+    getEngine().loadTexture(mTexture);
   }
 
   @Override
   public void onLoadComplete() {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public Engine onLoadEngine() {
-    // TODO Auto-generated method stub
-    return null;
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera));
   }
-
 }
