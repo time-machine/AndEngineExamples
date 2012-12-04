80c0854c68b6b912fd2556b48b2b219dc72f8a15
kumabotz <kumabotz@gmail.com>
2012-12-04 21:31:35 +0800

    Split screen example also uses RatioResolutionPolicy

--- src/org/anddev/andengine/examples/SplitScreenExample.java
+++ src/org/anddev/andengine/examples/SplitScreenExample.java
@@ -6,7 +6,7 @@ import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.camera.ChaseCamera;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.SplitScreenEngineOptions;
-import org.anddev.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.FPSCounter;
 import org.anddev.andengine.entity.Scene;
 import org.anddev.andengine.entity.primitives.Rectangle;
@@ -94,7 +94,8 @@ IAccelerometerListener, IOnSceneTouchListener {
         CAMERA_HEIGHT / 2, null);
     mChaseCamera = secondCamera;
     return new SingleSceneSplitScreenEngine(new SplitScreenEngineOptions(true,
-        ScreenOrientation.LANDSCAPE, new FillResolutionPolicy(), firstCamera,
+        ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH * 2, CAMERA_HEIGHT), firstCamera,
         secondCamera, false));
   }
 
