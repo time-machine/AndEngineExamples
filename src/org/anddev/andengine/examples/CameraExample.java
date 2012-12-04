1916b2b8949385dfa20c217192be6b82bf16202d
kumabotz <kumabotz@gmail.com>
2012-12-04 20:53:35 +0800

    Add camera example

--- src/org/anddev/andengine/examples/CameraExample.java
+++ src/org/anddev/andengine/examples/CameraExample.java
@@ -1,33 +1,53 @@
 package org.anddev.andengine.examples;
 
 import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.extension.camera.BaseCameraGameActivity;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 
 public class CameraExample extends BaseCameraGameActivity {
+  private static final int CAMERA_WIDTH = 720;
+  private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
+  private Texture mTexture;
+  private TextureRegion mFaceTextureRegion;
 
   @Override
   public Scene onLoadScene() {
-    // TODO Auto-generated method stub
-    return null;
+    final Scene scene = new Scene(1);
+    scene.setBackgroundColor(0, 0, 0, 0);
+    final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
+    final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
+    final Sprite face = new Sprite(x, y, mFaceTextureRegion);
+    scene.getTopLayer().addEntity(face);
+
+    return scene;
   }
 
   @Override
   public void onLoadResources() {
-    // TODO Auto-generated method stub
-
+    mTexture = new Texture(64, 32);
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "gfx/box_face.png", 0, 0);
+    getEngine().getTextureManager().loadTexture(mTexture);
   }
 
   @Override
   public void onLoadComplete() {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
   public Engine onLoadEngine() {
-    // TODO Auto-generated method stub
-    return null;
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera));
   }
-
 }
