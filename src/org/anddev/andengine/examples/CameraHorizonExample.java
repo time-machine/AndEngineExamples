8de1577ac1fa593ce7af2e40a7b852438058886f
kumabotz <kumabotz@gmail.com>
2012-12-04 21:02:16 +0800

    Add Camera horizon example, both camera examples not working, physic box related examples not working also

--- src/org/anddev/andengine/examples/CameraHorizonExample.java
+++ src/org/anddev/andengine/examples/CameraHorizonExample.java
@@ -1,42 +1,67 @@
 package org.anddev.andengine.examples;
 
 import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.extension.camera.BaseCameraGameActivity;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.sensor.orientation.IOrientationListener;
 import org.anddev.andengine.sensor.orientation.OrientationData;
 
+import android.util.Log;
+
 public class CameraHorizonExample extends BaseCameraGameActivity implements
 IOrientationListener {
+  private static final int CAMERA_WIDTH = 720;
+  private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
+  private Texture mTexture;
+  private TextureRegion mFaceTextureRegion;
+  private Sprite mFace;
 
   @Override
   public Scene onLoadScene() {
-    // TODO Auto-generated method stub
-    return null;
+    final Scene scene = new Scene(1);
+    scene.setBackgroundColor(0, 0, 0, 0);
+    final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
+    final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
+    mFace = new Sprite(x, y, mFaceTextureRegion);
+    scene.getTopLayer().addEntity(mFace);
+
+    return scene;
   }
 
   @Override
   public void onLoadResources() {
-    // TODO Auto-generated method stub
-
+    mTexture = new Texture(64, 32);
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "gfx/box_face.png", 0, 0);
+    getEngine().getTextureManager().loadTexture(mTexture);
   }
 
   @Override
   public void onLoadComplete() {
-    // TODO Auto-generated method stub
-
+    enableOrientationSensor(this);
   }
 
   @Override
   public Engine onLoadEngine() {
-    // TODO Auto-generated method stub
-    return null;
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera));
   }
 
   @Override
   public void onOrientationChanged(final OrientationData pOrientationData) {
-    // TODO Auto-generated method stub
-
+    final float roll = pOrientationData.getRoll();
+    Log.d("CameraHorizonExample", "Roll: " + pOrientationData.getRoll());
+    mFace.setPosition(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2 + (roll - 40) * 5);
   }
-
 }
