6b56c97c48999828c531efdbebdb5ebbb15ff720
kumabotz <kumabotz@gmail.com>
2012-12-04 22:13:56 +0800

    --adment

---
+++ src/org/anddev/andengine/examples/AugmentedRealityExample.java
@@ -0,0 +1,53 @@
+package org.anddev.andengine.examples;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.extension.augmentedreality.BaseAugmentedRealityGameActivity;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+
+public class AugmentedRealityExample extends BaseAugmentedRealityGameActivity {
+  private static final int CAMERA_WIDTH = 720;
+  private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
+  private Texture mTexture;
+  private TextureRegion mFaceTextureRegion;
+
+  @Override
+  public Scene onLoadScene() {
+    final Scene scene = new Scene(1);
+    scene.setBackgroundColor(0, 0, 0, 0);
+    final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
+    final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
+    final Sprite face = new Sprite(x, y, mFaceTextureRegion);
+    scene.getTopLayer().addEntity(face);
+
+    return scene;
+  }
+
+  @Override
+  public void onLoadResources() {
+    mTexture = new Texture(64, 32);
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "gfx/box_face.png", 0, 0);
+    getEngine().getTextureManager().loadTexture(mTexture);
+  }
+
+  @Override
+  public void onLoadComplete() {
+  }
+
+  @Override
+  public Engine onLoadEngine() {
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera, false));
+  }
+}
