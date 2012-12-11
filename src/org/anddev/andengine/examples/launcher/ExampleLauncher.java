1683e845cfc59de2c46e6772ed348f9c27d12f37
kumabotz <kumabotz@gmail.com>
2012-12-11 16:11:34 +0800

    Add button linking to AndEngine homepage

--- src/org/anddev/andengine/examples/launcher/ExampleLauncher.java
+++ src/org/anddev/andengine/examples/launcher/ExampleLauncher.java
@@ -1,9 +1,13 @@
 package org.anddev.andengine.examples.launcher;
 
+import org.anddev.andengine.examples.R;
+
 import android.app.ExpandableListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ExpandableListView;
 
 public class ExampleLauncher extends ExpandableListActivity {
@@ -12,9 +16,21 @@ public class ExampleLauncher extends ExpandableListActivity {
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.list_examples);
+
     mExpandableExampleLauncherListAdapter =
         new ExpandableExampleLauncherListAdapter(this);
+
     setListAdapter(mExpandableExampleLauncherListAdapter);
+
+    findViewById(R.id.btn_get_involved).setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(final View v) {
+        ExampleLauncher.this.startActivity(
+            new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.andengine.org")));
+      }
+    });
   }
 
   @Override
