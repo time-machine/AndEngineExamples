dcbd84a84835ddaedeb1d16813004118e95429b2
kumabotz <kumabotz@gmail.com>
2012-12-11 23:53:15 +0800

    Add welcome dialog show only on the very 1st start

--- src/org/anddev/andengine/examples/launcher/ExampleLauncher.java
+++ src/org/anddev/andengine/examples/launcher/ExampleLauncher.java
@@ -2,8 +2,12 @@ package org.anddev.andengine.examples.launcher;
 
 import org.anddev.andengine.examples.R;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ExpandableListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +16,9 @@ import android.widget.ExpandableListView;
 
 public class ExampleLauncher extends ExpandableListActivity {
   private ExpandableExampleLauncherListAdapter mExpandableExampleLauncherListAdapter;
+  private static final int DIALOG_FIRST_APP_LAUNCH = 0;
 
+  @SuppressWarnings("deprecation")
   @Override
   protected void onCreate(final Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -31,6 +37,27 @@ public class ExampleLauncher extends ExpandableListActivity {
             new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.andengine.org")));
       }
     });
+
+    if (isFirstTime("first.app.launch")) {
+      showDialog(DIALOG_FIRST_APP_LAUNCH);
+    }
+  }
+
+
+  @Override
+  @Deprecated
+  protected Dialog onCreateDialog(final int id) {
+    switch (id) {
+    case DIALOG_FIRST_APP_LAUNCH:
+      return new AlertDialog.Builder(this)
+        .setTitle(R.string.dialog_first_app_launch_title)
+        .setMessage(R.string.dialog_first_app_launch_message)
+        .setIcon(android.R.drawable.ic_dialog_info)
+        .setPositiveButton(android.R.string.ok, null)
+        .create();
+    default:
+      return super.onCreateDialog(id);
+    }
   }
 
   @Override
@@ -40,5 +67,14 @@ public class ExampleLauncher extends ExpandableListActivity {
         groupPosition, childPosition);
     startActivity(new Intent(this, example.CLASS));
     return super.onChildClick(parent, v, groupPosition, childPosition, id);
+  }
+
+  public boolean isFirstTime(final String pKey) {
+    final SharedPreferences prefs = getPreferences(Context.MODE_PRIVATE);
+    if (prefs.getBoolean(pKey, true)) {
+      prefs.edit().putBoolean(pKey, false).commit();
+      return true;
+    }
+    return false;
   }
 }
