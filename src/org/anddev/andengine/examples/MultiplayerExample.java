a25994e825e35ae8d25e25b3961037815e1b6808
kumabotz <kumabotz@gmail.com>
2012-12-12 20:44:54 +0800

    Add physic remove example

--- src/org/anddev/andengine/examples/MultiplayerExample.java
+++ src/org/anddev/andengine/examples/MultiplayerExample.java
@@ -4,7 +4,13 @@ import java.io.IOException;
 import java.net.Socket;
 
 import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.FPSCounter;
 import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.extension.multiplayer.protocol.adt.cmd.AbstractCommand;
 import org.anddev.andengine.extension.multiplayer.protocol.adt.cmd.connection.ConnectionCloseCommand;
 import org.anddev.andengine.extension.multiplayer.protocol.adt.feedback.AbstractServerFeedback;
@@ -19,11 +25,14 @@ import org.anddev.andengine.extension.multiplayer.protocol.server.BaseClientConn
 import org.anddev.andengine.extension.multiplayer.protocol.shared.AbstractConnector;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.util.Debug;
 
 public class MultiplayerExample extends BaseExampleGameActivity {
   private static final int CAMERA_WIDTH = 720;
   private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
 
   private Texture mTexture;
   private TextureRegion mFaceTextureRegion;
@@ -113,19 +122,35 @@ public class MultiplayerExample extends BaseExampleGameActivity {
       Debug.e("Error", t);
     }
 
-    mCamera = new Camera()
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera, false));
   }
 
   @Override
   public void onLoadResources() {
-    // TODO Auto-generated method stub
-
+    mTexture = new Texture(64, 32);
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "gfx/boxface.png", 0, 0);
+    getEngine().getTextureManager().loadTexture(mTexture);
   }
 
   @Override
   public Scene onLoadScene() {
-    // TODO Auto-generated method stub
-    return null;
+    getEngine().registerPreFrameHandler(new FPSCounter());
+
+    final Scene scene = new Scene(1);
+    scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
+
+    // Calculate the coordinates for the face, so its centered on the camera
+    final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
+    final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
+
+    // Create the face and add it to the scene
+    final Sprite face = new Sprite(x, y, mFaceTextureRegion);
+    scene.getTopLayer().addEntity(face);
+
+    return scene;
   }
 
   @Override
