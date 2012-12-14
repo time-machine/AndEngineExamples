2f3b8def62e15ad3408f58ddd85ae7fc9add48c3
kumabotz <kumabotz@gmail.com>
2012-12-14 17:03:49 +0800

    Add touch drag many example using a deck of cards

--- src/org/anddev/andengine/examples/MultiplayerExample.java
+++ src/org/anddev/andengine/examples/MultiplayerExample.java
@@ -26,23 +26,34 @@ import org.anddev.andengine.extension.multiplayer.protocol.server.BaseServer.ISe
 import org.anddev.andengine.extension.multiplayer.protocol.server.ClientConnector;
 import org.anddev.andengine.extension.multiplayer.protocol.server.ClientMessageExtractor;
 import org.anddev.andengine.extension.multiplayer.protocol.shared.BaseConnector;
+import org.anddev.andengine.extension.multiplayer.protocol.util.IPUtils;
 import org.anddev.andengine.input.touch.IOnSceneTouchListener;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.util.Debug;
 
-import android.os.Handler;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.MotionEvent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MultiplayerExample extends BaseExampleGameActivity {
   private static final int CAMERA_WIDTH = 720;
   private static final int CAMERA_HEIGHT = 480;
 
-  private static final String SERVER_IP = "127.0.0.1";
   private static final int SERVER_PORT = 4444;
 
   private static final short FLAG_ADD_FACE = 1;
+
+  private static final int DIALOG_CHOOSE_SERVER_OR_CLIENT_ID = 0;
+  private static final int DIALOG_ENTER_SERVER_IP_ID =
+      DIALOG_CHOOSE_SERVER_OR_CLIENT_ID + 1;
+  private static final int DIALOG_SHOW_SERVER_IP_ID =
+      DIALOG_ENTER_SERVER_IP_ID + 1;
 
   private Camera mCamera;
 
@@ -52,73 +63,74 @@ public class MultiplayerExample extends BaseExampleGameActivity {
   private BaseServer mServer;
   private ServerConnector mServerConnector;
 
+  private String mServerIP = "127.0.0.1";
+
+
+  @Override
+  @Deprecated
+  protected Dialog onCreateDialog(final int id) {
+    switch (id) {
+    case DIALOG_SHOW_SERVER_IP_ID:
+      return new AlertDialog.Builder(this)
+      .setIcon(android.R.drawable.ic_dialog_info)
+      .setTitle("Your Server-IP ...")
+      .setMessage("The IP of your Server is:\n" + IPUtils.getIPAddress(this))
+      .setPositiveButton(android.R.string.ok, null)
+      .create();
+    case DIALOG_ENTER_SERVER_IP_ID:
+      final EditText ipEditText = new EditText(this);
+      return new AlertDialog.Builder(this)
+      .setIcon(android.R.drawable.ic_dialog_info)
+      .setTitle("Enter Server-IP ...")
+      .setView(ipEditText)
+      .setPositiveButton("Connect", new OnClickListener() {
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+          mServerIP = ipEditText.getText().toString();
+          initClient();
+        }
+      })
+      .setNegativeButton(android.R.string.cancel, new OnClickListener() {
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+          finish();
+        }
+      })
+      .create();
+    case DIALOG_CHOOSE_SERVER_OR_CLIENT_ID:
+      return new AlertDialog.Builder(this)
+      .setIcon(android.R.drawable.ic_dialog_info)
+      .setTitle("Be Server or Client ...")
+      .setPositiveButton("Client", new OnClickListener() {
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+          showDialog(DIALOG_ENTER_SERVER_IP_ID);
+        }
+      })
+      .setNeutralButton("Server", new OnClickListener() {
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+          initServer();
+          showDialog(DIALOG_SHOW_SERVER_IP_ID);
+        }
+      })
+      .setNegativeButton("Both", new OnClickListener() {
+        @Override
+        public void onClick(final DialogInterface dialog, final int which) {
+          initServerAndClient();
+          showDialog(DIALOG_SHOW_SERVER_IP_ID);
+        }
+      })
+      .create();
+    default:
+      return super.onCreateDialog(id);
+    }
+  }
+
+  @SuppressWarnings("deprecation")
   @Override
   public Engine onLoadEngine() {
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        MultiplayerExample.this.initServer();
-
-        // wait some time after the server has been started, so it actually can
-        // start up
-        try {
-          Thread.sleep(500);
-        }
-        catch (final Throwable t) {
-          Debug.e("Error", t);
-        }
-
-        MultiplayerExample.this.initClient();
-      }
-    }, 500);
-//    mServer = new AbstractServer(4444,
-//        new AbstractClientConnectionListener() {
-//          @Override
-//          protected void onConnectInner(final AbstractConnector<AbstractCommand> pConnector) {
-//            MultiplayerExample.this.log("Client connected: " + pConnector.getSocket());
-//          }
-//
-//          @Override
-//          protected void onDisconnectInner(final AbstractConnector<AbstractCommand> pConnector) {
-//            MultiplayerExample.this.log("Client disconnected: " + pConnector.getSocket());
-//          }
-//        },
-//        new IServerStateListener() {
-//          @Override
-//          public void onTerminated(final int pPort) {
-//          }
-//
-//          @Override
-//          public void onStarted(final int pPort) {
-//          }
-//
-//          @Override
-//          public void onException(final Throwable pThrowable) {
-//          }
-//        }) {
-//      @Override
-//      protected BaseClientConnector newClientConnector(final Socket pClientSocket,
-//          final AbstractClientConnectionListener pClientConnectionListener)
-//              throws Exception {
-//        return new BaseClientConnector(pClientSocket, pClientConnectionListener,
-//            new AbstractCommandSwitch() {
-//              @Override
-//              public void doSwitch(final AbstractCommand pCommand)
-//                  throws IOException {
-//                MultiplayerExample.this.log("Command received: " +
-//                    pCommand.toString());
-//              }
-//            });
-//      }
-//    };
-//    mServer.start();
-//
-//
-//
-//
-//    catch (final Throwable t) {
-//      Debug.e("Error", t);
-//    }
+    showDialog(DIALOG_CHOOSE_SERVER_OR_CLIENT_ID);
 
     mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
     return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
@@ -153,22 +165,18 @@ public class MultiplayerExample extends BaseExampleGameActivity {
     final Scene scene = new Scene(1);
     scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
 
-    // Calculate the coordinates for the face, so its centered on the camera
-    final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
-    final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
-
-    addFace(scene, x, y);
-
     scene.setOnSceneTouchListener(new IOnSceneTouchListener() {
       @Override
       public boolean onSceneTouchEvent(final Scene pScene, final MotionEvent pSceneMotionEvent) {
         if (pSceneMotionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-          try {
-            mServer.sendBroadcastServerMessage(new AddFaceServerMessage(
+          if (mServer != null) {
+            try {
+              mServer.sendBroadcastServerMessage(new AddFaceServerMessage(
                 pSceneMotionEvent.getX(), pSceneMotionEvent.getY()));
-          }
-          catch (final IOException e) {
-            Debug.e(e);
+            }
+            catch (final IOException e) {
+              Debug.e(e);
+            }
           }
           return true;
         }
@@ -189,8 +197,33 @@ public class MultiplayerExample extends BaseExampleGameActivity {
     pScene.getTopLayer().addEntity(face);
   }
 
-  private static void log(final String pMessage) {
+  private void log(final String pMessage) {
     Debug.d(pMessage);
+  }
+
+  private void toast(final String pMessage) {
+    runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        Toast.makeText(MultiplayerExample.this, pMessage, Toast.LENGTH_SHORT)
+            .show();
+      }
+    });
+  }
+
+  private void initServerAndClient() {
+    initServer();
+
+    // Wait some time after the server has been started, so it actually can
+    // start up
+    try {
+      Thread.sleep(500);
+    }
+    catch (final Throwable t) {
+      Debug.e("Error", t);
+    }
+
+    initClient();
   }
 
   private void initServer() {
@@ -212,8 +245,8 @@ public class MultiplayerExample extends BaseExampleGameActivity {
                   @Override
                   public void doSwitch(final BaseClientMessage pClientMessage)
                       throws IOException {
-                    MultiplayerExample.log("SERVER: ClientMessage received: " +
-                      pClientMessage.toString());
+                    log("SERVER: ClientMessage received: " +
+                        pClientMessage.toString());
                   }
                 }
             );
@@ -222,10 +255,9 @@ public class MultiplayerExample extends BaseExampleGameActivity {
     mServer.start();
   }
 
-
   private void initClient() {
     try {
-      mServerConnector= new ServerConnector(new Socket(SERVER_IP, SERVER_PORT),
+      mServerConnector= new ServerConnector(new Socket(mServerIP, SERVER_PORT),
         new ExampleServerConnectionListener(),
         new ServerMessageExtractor() {
           @Override
@@ -250,7 +282,7 @@ public class MultiplayerExample extends BaseExampleGameActivity {
                   addFaceServerMessage.mY);
               break;
             default:
-              MultiplayerExample.log("CLIENT: ServerMessage received: " +
+                log("CLIENT: ServerMessage received: " +
                   pServerMessage.toString());
             }
           }
@@ -296,46 +328,46 @@ public class MultiplayerExample extends BaseExampleGameActivity {
     }
   }
 
-  private static class ExampleServerConnectionListener
+  private class ExampleServerConnectionListener
       extends BaseServerConnectionListener {
     @Override
     protected void onConnectInner(final BaseConnector<BaseServerMessage> pConnector) {
-      MultiplayerExample.log("CLIENT: Connected to server.");
+      toast("CLIENT: Connected to server.");
     }
 
     @Override
     protected void onDisconnectInner(final BaseConnector<BaseServerMessage> pConnector) {
-      MultiplayerExample.log("CLIENT: Disconnected from server...");
+      log("CLIENT: Disconnected from server...");
     }
   }
 
-  private static class ExampleServerStateListener implements IServerStateListener {
+  private class ExampleServerStateListener implements IServerStateListener {
     @Override
     public void onStarted(final int pPort) {
-      MultiplayerExample.log("SERVER: Started at port: " + pPort);
+      log("SERVER: Started at port: " + pPort);
     }
 
     @Override
     public void onTerminated(final int pPort) {
-      MultiplayerExample.log("SERVER: Terminated at port: " + pPort);
+      log("SERVER: Terminated at port: " + pPort);
     }
 
     @Override
     public void onException(final Throwable pThrowable) {
-      MultiplayerExample.log("SERVER: Exception: " + pThrowable);
+      log("SERVER: Exception: " + pThrowable);
     }
   }
 
-  private static class ExampleClientConnectionListener
+  private class ExampleClientConnectionListener
       extends BaseClientConnectionListener {
     @Override
     protected void onConnectInner(final BaseConnector<BaseClientMessage> pConnector) {
-      MultiplayerExample.log("SERVER: Client connected:" + pConnector.getSocket());
+      toast("SERVER: Client connected:" + pConnector.getSocket());
     }
 
     @Override
     protected void onDisconnectInner(final BaseConnector<BaseClientMessage> pConnector) {
-      MultiplayerExample.log("SERVER: Client disconnected:" + pConnector.getSocket());
+      toast("SERVER: Client disconnected:" + pConnector.getSocket());
     }
   }
 }
--- src/org/anddev/andengine/examples/TouchDragManyExample.java
+++ src/org/anddev/andengine/examples/TouchDragManyExample.java
@@ -1,33 +1,126 @@
 package org.anddev.andengine.examples;
 
+import java.util.HashMap;
+
 import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.FPSCounter;
 import org.anddev.andengine.entity.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.examples.adt.card.Card;
+import org.anddev.andengine.input.touch.IOnAreaTouchListener;
+import org.anddev.andengine.input.touch.IOnSceneTouchListener;
+import org.anddev.andengine.input.touch.ITouchArea;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
+import android.view.MotionEvent;
+
 public class TouchDragManyExample extends BaseGameActivity {
+  private static final int CAMERA_WIDTH = 720;
+  private static final int CAMERA_HEIGHT = 480;
+
+  private Camera mCamera;
+  private Texture mCardDeckTexture;
+
+  protected Sprite mSelectedSprite;
+
+  private HashMap<Card, TextureRegion> mCardToTextureRegionMap;
 
   @Override
   public Engine onLoadEngine() {
-    // TODO Auto-generated method stub
-    return null;
+    mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+    return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
+        new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), mCamera, false));
   }
 
   @Override
   public void onLoadResources() {
-    // TODO Auto-generated method stub
+    mCardDeckTexture = new Texture(1024, 512);
+    TextureRegionFactory.createFromAsset(mCardDeckTexture, this,
+        "gfx/carddeck_tiled.png", 0, 0);
+    mCardToTextureRegionMap = new HashMap<Card, TextureRegion>();
 
+    // extract the TextureRegion of each card in the whole deck
+    for (final Card card : Card.values()) {
+      final TextureRegion cardTextureRegion = TextureRegionFactory.extractFromTexture(
+          mCardDeckTexture, card.getTexturePositionX(),
+          card.getTexturePositionY(), Card.CARD_WIDTH, Card.CARD_HEIGHT);
+      mCardToTextureRegionMap.put(card, cardTextureRegion);
+    }
+
+    getEngine().getTextureManager().loadTexture(mCardDeckTexture);
   }
 
   @Override
   public Scene onLoadScene() {
-    // TODO Auto-generated method stub
-    return null;
+    getEngine().registerPreFrameHandler(new FPSCounter());
+
+    final Scene scene = new Scene(1);
+
+    addCard(scene, Card.CLUB_ACE, 200, 100);
+    addCard(scene, Card.HEART_ACE, 200, 260);
+    addCard(scene, Card.DIAMOND_ACE, 440, 100);
+    addCard(scene, Card.SPADE_ACE, 440, 260);
+
+    scene.setBackgroundColor(0.09804f, 0.6274f, 0.8784f);
+
+    scene.setOnSceneTouchListener(new IOnSceneTouchListener() {
+      @Override
+      public boolean onSceneTouchEvent(final Scene pScene,
+          final MotionEvent pSceneMotionEvent) {
+        return updateSelectedCardPosition(pSceneMotionEvent);
+      }
+    });
+
+    scene.setOnAreaTouchListener(new IOnAreaTouchListener() {
+      @Override
+      public boolean onAreaTouched(final ITouchArea pTouchArea,
+          final MotionEvent pSceneMotionEvent) {
+        if (pSceneMotionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+          mSelectedSprite = (Sprite)pTouchArea;
+          mSelectedSprite.setScale(1.2f);
+          return true;
+        }
+        else {
+          return updateSelectedCardPosition(pSceneMotionEvent);
+        }
+      }
+    });
+
+    return scene;
   }
 
   @Override
   public void onLoadComplete() {
-    // TODO Auto-generated method stub
-
   }
 
+  protected boolean updateSelectedCardPosition(final MotionEvent pSceneMotionEvent) {
+    if (mSelectedSprite != null) {
+      if (pSceneMotionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+        mSelectedSprite.setPosition(pSceneMotionEvent.getX() - Card.CARD_WIDTH / 2,
+            pSceneMotionEvent.getY() - Card.CARD_HEIGHT / 2);
+      }
+      else if (pSceneMotionEvent.getAction() == MotionEvent.ACTION_UP) {
+        mSelectedSprite.setScale(1.0f);
+        mSelectedSprite = null;
+      }
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  private void addCard(final Scene pScene, final Card pCard, final int pX,
+      final int pY) {
+    final Sprite sprite = new Sprite(pX, pY, mCardToTextureRegionMap.get(pCard));
+    pScene.getTopLayer().addEntity(sprite);
+    pScene.registerTouchArea(sprite);
+  }
 }
