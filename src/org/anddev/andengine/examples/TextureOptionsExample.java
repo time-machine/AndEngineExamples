7654d6a448f6dac1b647ef5d5d0d05b7d2ebd42f
kumabotz <kumabotz@gmail.com>
2012-12-25 22:03:45 +0800

    TextureOptionsExample shows the usage of repeating Textures

--- src/org/anddev/andengine/examples/TextureOptionsExample.java
+++ src/org/anddev/andengine/examples/TextureOptionsExample.java
@@ -5,13 +5,14 @@ import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.layer.ILayer;
 import org.anddev.andengine.entity.scene.Scene;
-import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
-import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 
 public class TextureOptionsExample extends BaseExample {
   private static final int CAMERA_WIDTH = 720;
@@ -20,8 +21,12 @@ public class TextureOptionsExample extends BaseExample {
   private Camera mCamera;
   private Texture mTexture;
   private Texture mTextureBilinear;
-  private TiledTextureRegion mFaceTextureRegion;
-  private TiledTextureRegion mFaceTextureRegionBilinear;
+
+  private Texture mTextureRepeating;
+
+  private TextureRegion mFaceTextureRegion;
+  private TextureRegion mFaceTextureRegionBilinear;
+  private TextureRegion mFaceTextureRegionRepeating;
 
   @Override
   public Scene onLoadScene() {
@@ -32,29 +37,51 @@ public class TextureOptionsExample extends BaseExample {
     final int x = (CAMERA_WIDTH - mFaceTextureRegion.getWidth()) / 2;
     final int y = (CAMERA_HEIGHT - mFaceTextureRegion.getHeight()) / 2;
 
-    final AnimatedSprite face = new AnimatedSprite(x - 120, y,
-        mFaceTextureRegion);
+    final Sprite face = new Sprite(x - 160, y - 40, mFaceTextureRegion);
     face.setScale(4);
 
-    final AnimatedSprite faceBillinear = new AnimatedSprite(x + 120, y,
+    final Sprite faceBillinear = new Sprite(x + 160, y - 40,
         mFaceTextureRegionBilinear);
     faceBillinear.setScale(4);
 
-    scene.getTopLayer().addEntity(face);
-    scene.getTopLayer().addEntity(faceBillinear);
+    // make sure sprite has the same size as mTextureRegionRepeating
+    // giving the sprite twice the height shows you'd also have to change the
+    // height of the TextureRegion!
+    final Sprite faceRepeating = new Sprite(x - 160, y + 100,
+        mFaceTextureRegionRepeating.getWidth(),
+        mFaceTextureRegionRepeating.getHeight() * 2,
+        mFaceTextureRegionRepeating);
+
+    final ILayer topLayer = scene.getTopLayer();
+    topLayer.addEntity(face);
+    topLayer.addEntity(faceBillinear);
+    topLayer.addEntity(faceRepeating);
 
     return scene;
   }
 
   @Override
   public void onLoadResources() {
-    mTexture = new Texture(64, 32, TextureOptions.DEFAULT);
-    mTextureBilinear = new Texture(64, 32, TextureOptions.BILINEAR);
-    mFaceTextureRegion = TextureRegionFactory.createTiledFromAsset(mTexture,
-        this, "gfx/boxface_tiled.png", 0, 0, 2, 1);
-    mFaceTextureRegionBilinear = TextureRegionFactory.createTiledFromAsset(
-        mTextureBilinear, this, "gfx/boxface_tiled.png", 0, 0, 2, 1);
-    getEngine().getTextureManager().loadTextures(mTexture, mTextureBilinear);
+    TextureRegionFactory.setAssetBasePath("gfx/");
+    mTexture = new Texture(32, 32, TextureOptions.DEFAULT);
+    mFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
+        "boxface.png", 0, 0);
+
+    mTextureBilinear = new Texture(32, 32, TextureOptions.BILINEAR);
+    mFaceTextureRegionBilinear = TextureRegionFactory.createFromAsset(
+        mTextureBilinear, this, "boxface.png", 0, 0);
+
+    mTextureRepeating = new Texture(32, 32, TextureOptions.BILINEAR);
+    mFaceTextureRegionRepeating = TextureRegionFactory.createFromAsset(
+        mTextureRepeating, this, "boxface.png", 0, 0);
+
+    // the following statement causes the Texture to be printed horizontally
+    // 10x on any Sprite that uses it. so we will later increase the width of
+    // such a sprite by the same factor to avoid distortion
+    mFaceTextureRegionRepeating.setWidth(
+        10 * mFaceTextureRegionRepeating.getWidth());
+    mEngine.getTextureManager().loadTextures(mTexture, mTextureBilinear,
+        mTextureRepeating);
   }
 
   @Override
