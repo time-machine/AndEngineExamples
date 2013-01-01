4e238a2d8d4204e048eed3b659a3097f4a20f54c
kumabotz <kumabotz@gmail.com>
2013-01-02 00:28:25 +0800

    Fix sound example

--- src/org/anddev/andengine/examples/SoundExample.java
+++ src/org/anddev/andengine/examples/SoundExample.java
@@ -17,6 +17,7 @@ import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
 import org.anddev.andengine.input.touch.TouchEvent;
 import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
 import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.util.Debug;
@@ -26,44 +27,47 @@ import android.widget.Toast;
 
 public class SoundExample extends BaseExample {
   private static final int CAMERA_WIDTH = 720;
-  private static final int CAMERA_HEIGHT= 480;
+  private static final int CAMERA_HEIGHT = 480;
 
   private Texture mTexture;
   private TextureRegion mTankTextureRegion;
+
   private Sound mExplosionSound;
 
   @Override
   public Engine onLoadEngine() {
-    Toast.makeText(this, "Touch the tank to hear a shot sound.",
+    Toast.makeText(this, "Touch the tank to hear an explosion sound.",
         Toast.LENGTH_LONG).show();
     final Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
     return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE,
         new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), camera)
-        .setNeedsMusic(true));
+            .setNeedsSound(true));
   }
 
   @Override
   public void onLoadResources() {
-    mTexture = new Texture(128, 256);
+    mTexture = new Texture(128, 256, TextureOptions.BILINEAR);
     TextureRegionFactory.setAssetBasePath("gfx/");
     mTankTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this,
         "tank.png", 0, 0);
 
     SoundFactory.setAssetBasePath("mfx/");
+
     try {
       mExplosionSound = SoundFactory.createSoundFromAsset(
-          getEngine().getSoundManager(), this, "explosion.ogg");
+          mEngine.getSoundManager(), this, "explosion.ogg");
     }
     catch (final IOException e) {
       Debug.e("Error", e);
     }
 
-    getEngine().getTextureManager().loadTexture(mTexture);
+    mEngine.getTextureManager().loadTexture(mTexture);
   }
 
   @Override
   public Scene onLoadScene() {
-    getEngine().registerPreFrameHandler(new FPSLogger());
+    mEngine.registerPreFrameHandler(new FPSLogger());
+
     final Scene scene = new Scene(1);
     scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
 
@@ -90,8 +94,5 @@ public class SoundExample extends BaseExample {
 
   @Override
   public void onLoadComplete() {
-    // TODO Auto-generated method stub
-
   }
-
 }
