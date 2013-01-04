af4da636e96a25055a5621260b2d457965eff970
kumabotz <kumabotz@gmail.com>
2013-01-04 11:21:26 +0800

    Change player moving path

--- src/org/anddev/andengine/examples/TMXTiledMapExample.java
+++ src/org/anddev/andengine/examples/TMXTiledMapExample.java
@@ -103,9 +103,8 @@ public class TMXTiledMapExample extends BaseExample {
         mPlayerTextureRegion);
     mChaseCamera.setChaseShape(player);
 
-    final Path path = new Path(5).to(10, 10).to(10, CAMERA_HEIGHT - 74)
-        .to(CAMERA_WIDTH - 58, CAMERA_HEIGHT - 74).to(CAMERA_WIDTH - 58, 10)
-        .to(10, 10);
+    final Path path = new Path(5).to(240, 160).to(240, 600).to(600, 600)
+        .to(600, 160).to(240, 160);
 
     // add the proper animation when a waypoint of the path is passed
     player.addShapeModifier(new LoopModifier(new PathModifier(30, path, null,
